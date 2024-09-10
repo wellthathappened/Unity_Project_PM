a28e9076388800e82fb6804bbe56eca555959b1c
IAN LEWIS <33504743+wellthathappened@users.noreply.github.com>
2024-09-10 13:43:45 -0700

    Completed player controller with sprint (& Sprint toggle option)

--- Unity_Intro_PM/Assets/Scripts/PlayerController.cs
+++ Unity_Intro_PM/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,80 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody myRB;
+    Camera playerCam;
 
+    Vector2 camRotation;
+
+    public bool sprintMode = false;
+
+    [Header("Movement Settings")]
     public float speed = 10.0f;
+    public float sprintMultiplier = 2.5f;
     public float jumpHeight = 5.0f;
+    public float groundDetectDistance = 1f;
+
+    [Header("User Settings")]
+    public bool sprintToggleOption = false;
+    public float mouseSensitivity = 2.0f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
+    public float camRotationLimit = 90f;
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        playerCam = transform.GetChild(0).GetComponent<Camera>();
+
+        camRotation = Vector2.zero;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
+
+        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
         Vector3 temp = myRB.velocity;
 
-        temp.x = Input.GetAxisRaw("Vertical") * speed;
-        temp.z = Input.GetAxisRaw("Horizontal") * speed;
+        float verticalMove = Input.GetAxisRaw("Vertical");
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(!sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+                sprintMode = true;
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+                sprintMode = false;
+        }
+
+        if(sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && verticalMove > 0)
+                sprintMode = true;
+
+            if (verticalMove <= 0)
+                sprintMode = false;
+        }
+
+        if(!sprintMode)
+            temp.x = verticalMove * speed;
+
+        if(sprintMode)
+            temp.x = verticalMove * speed * sprintMultiplier;
+
+        temp.z = horizontalMove * speed;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDetectDistance))
             temp.y = jumpHeight;
 
         myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
