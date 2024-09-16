c68f1c7a297de935a72ca61f38a546cd28b9523e
IAN LEWIS <33504743+wellthathappened@users.noreply.github.com>
2024-09-16 13:45:35 -0700

    Added health + pickup & weapon placement

--- Unity_Intro_PM/Assets/Scripts/PlayerController.cs
+++ Unity_Intro_PM/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,20 @@ public class PlayerController : MonoBehaviour
 
     Vector2 camRotation;
 
-    public bool sprintMode = false;
+    public Transform weaponSlot;
+
+    [Header("Player Stats")]
+    public int maxHealth = 5;
+    public int health = 5;
+    public int healthRestore = 1;
+
+    [Header("Weapon Stats")]
+    public bool canFire = true;
 
     [Header("Movement Settings")]
     public float speed = 10.0f;
     public float sprintMultiplier = 2.5f;
+    public bool sprintMode = false;
     public float jumpHeight = 5.0f;
     public float groundDetectDistance = 1f;
 
@@ -82,5 +91,27 @@ public class PlayerController : MonoBehaviour
             temp.y = jumpHeight;
 
         myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if((health < maxHealth) && collision.gameObject.tag == "healthPickup")
+        {
+            health += healthRestore;
+
+            if (health > maxHealth)
+                health = maxHealth;
+
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "weapon")
+            collision.gameObject.transform.SetParent(weaponSlot);
+    }
+
+    IEnumerator cooldownFire(float time)
+    {
+        yield return new WaitForSeconds(time);
+        canFire = true;
     }
 }
